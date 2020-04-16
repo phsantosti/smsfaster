2ec396a3631a65cb3b0074451f22453c972718bb
Pedro Santos <phsantosti@gmail.com>
2020-04-16 10:21:49 -0300

    Initial commit

---
+++ app/src/main/java/br/com/pedrohsantos/smsfaster/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.pedrohsantos.smsfaster
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
